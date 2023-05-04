07fae71142bbd60b9c09c168933a0db98140dfc2
beratakkaya1 <beratakkaya@hotmail.com.tr>
2023-05-04 13:55:21 +0300

    review

--- src/main/java/main/entity/Book.java
+++ src/main/java/main/entity/Book.java
@@ -15,7 +15,6 @@ import javax.persistence.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Book {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -24,41 +23,6 @@ public class Book {
 
     private Integer publicationYear;
 
-
     @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private Author author;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Integer getPublicationYear() {
-        return publicationYear;
-    }
-
-    public void setPublicationYear(Integer publicationYear) {
-        this.publicationYear = publicationYear;
-    }
-
-    public Author getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(Author author) {
-        this.author = author;
-    }
-
-    // getters and setters
 }
--- src/main/java/main/repository/BookRepository.java
+++ src/main/java/main/repository/BookRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-
     List<Book> findByAuthor_Name(String name);
 }
